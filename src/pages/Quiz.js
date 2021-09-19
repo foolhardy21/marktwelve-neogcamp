688adecd8882db7316388dd059839aac99db35ef
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-20 02:13:21 +0530

    style - quiz layout

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import questions from './quizdata'
 
 const Quiz = () => {
+
+    function calculateScore(e) {
+        e.preventDefault()
+        console.log(e.target)
+    }
     return (
-        <div>Quiz page</div>
+        <div>
+            <h3>Quiz on triangles</h3>
+            <form onSubmit={calculateScore}>
+                {
+                    questions.map((qset, index1) => {
+                        return (
+                            <div key={index1}>
+                                {qset.ques}
+                                <div>
+                                {
+                                    qset.options.map((option, index) => {
+                                        return <label key={index} htmlFor={`option${index}`}>
+                                                <input id={`option${index}`} type='radio' name={`${index1}`} value={option}/>
+                                                {option}
+                                            </label>
+                                    })
+                                }
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <input type='submit' value='submit' />
+            </form>
+        </div>
     )
 }
 export default Quiz
---
+++ src/pages/quizdata.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        ques: 'What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?',
+        options: ['45°', '90°', '60°'],
+        ans: '90°'
+    },
+    {
+        ques: 'What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?',
+        options: ['obtuse', 'acute', 'right angled'],
+        ans: 'right angled'
+    },
+    {
+        ques: 'A triangle can have',
+        options: ['one right angle', 'two right angle'],
+        ans: 'one right angle'
+    },
+    {
+        ques: 'The perimeter of scalene triangle with sides a, b, c is',
+        options: ['a+b+c', '2a', 'None of these'],
+        ans: 'a+b+c'
+    },
+    {
+        ques: 'Which of the following can form a right angled triangle?',
+        options: ['14,15,26', '12,16,20'],
+        ans: '12,16,20'
+    },
+]
+
+export default questions
